731c14af23fe73a9c6d1b69a2d380730793c5af4
AmyWilhoite <amywilhoite@gmail.com>
2022-03-15 00:26:05 -0400

    updates to html card and index.js code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // packages 
-// include jest
+// TODO: include jest
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateHTML = require('./src/generateHTML'); //get HTML template
@@ -10,15 +10,63 @@ const manager = require ("./lib/manager");
 const engineer = require ("./lib/engineer");
 const intern = require ("./lib/intern");
 
-// TODO: Create array for personnel data inputs
-const addPersonnel = [
-        {
-          type: 'input',
-          name: 'title',
-          message: 'What is your name?',
-        },
-        
+// TODO: identify output location
+
+// TODO: Create array for employee data inputs
+const addEmployee = [
+          {
+            type: 'list',
+            name: 'pickRole',
+            message: 'Which team member would you like to add?',
+            choices: [ 'Manager', 'Engineer', 'Intern'],
+          },       
+          {
+            type: 'input',
+            name: 'employeeID',
+            message: 'What is the team member employee ID?',
+          },
+          {
+            type: 'input',
+            name: 'name',
+            message: 'What is the team member name?',
+          },
+          {
+            type: 'input',
+            name: 'email',
+            message: 'What is the team member email?',
+          },
     ];
+
+ // if manager include office number + override role
+    {
+      type: 'input',
+      name: 'officePhone',
+      message: 'What is your office phone number?',
+    },
+    
+
+  // if engineer include Github + repository + over ride role
+    {
+      type: 'input',
+      name: 'gitHub',
+      message: 'What is your github username?',
+    },
+
+    {
+      type: 'input',
+      name: 'githubLink',
+      message: 'What is your github repository url?',
+    },
+
+
+    // if intern include school
+    {
+      type: 'input',
+      name: 'school',
+      message: 'What is your school name?',
+    },
+
+
 
 // function to write HTML file
 // syntax: fs.writeFile( file, data, options, callback )
@@ -30,7 +78,7 @@ function writeToFile(fileName, data) {
 // function to initialize app
 function init() {
     inquirer
-    .prompt(addPersonnel)
+    .prompt(addEmployee)
     .then ((data) => writeToFile(data.title, data))
 };
 
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,30 +1,41 @@
 // starter code
 // TODO: update html content & add cards
+
 function generateHTML(data) {
     return `${data.title}
     
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${name}</h1>
-    <p class="lead">I am from ${location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${github}</li>
-      <li class="list-group-item">LinkedIn: ${linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+      <title>Document</title>
+    </head>
+    <body>
+    <div class="card-group">
+      <div class="jumbotron-fluid">
+        <div class="card m-3 jumbotron-fluid">
+         <div class="card " style = "max-width:18rem;">
+         <div class="card-body">
+      <!-- // Employee -->
+          <h5 class="card-title">Role:${data.role}</h5>
+          <p class="card-text">Name: ${data.name}</p>
+          <p class="card-text">Email: ${data.email}</p> 
+        <!-- // Manager + override Role-->
+          <p class="card-text">Office Number: ${data.officePhone}</p>
+          <!-- // Engineer + override Role-->
+          <p class="card-text">GitHub Username: ${data.userName}</p>
+          <p class="card-text">GitHub Link: ${data.githubLink}</p>
+          <!-- // Intern + override Role-->
+          <p class="card-text">School: ${data.employeeID}</p>
+        <p class="card-text"><small class="text-muted">${data.employeeID}</small></p>
+        </div>
+        </div>
+      </div>
+    </div>
+    </body>
+    </html>
 `;
 }
 
