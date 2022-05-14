6a3477730e8a2ba059ea431faa56e7f0f39b6984
AmyWilhoite <amywilhoite@gmail.com>
2022-05-14 16:11:33 -0400

    revisions to index and generatehtml to get html to work with loop

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const inquirer = require('inquirer');
 
 
 // team profile
-const Intern = require ("./lib/intern");
+const Intern = require("./lib/intern");
 const Engineer = require('./lib/engineer');
 const Manager = require('./lib/manager');
 
@@ -15,125 +15,143 @@ const Manager = require('./lib/manager');
 let manager = [];
 let engineer = [];
 let intern = [];
-let addEmployee = {manager, engineer, intern};
+let addEmployee = { manager, engineer, intern };
 
 
 // TODO: Create array for employee data inputs, parent object
 // all employee have these basic fields
-function promptUser () {
+function promptUser() {
   return inquirer.prompt([
-          {
-            type: 'list',
-            name: 'pickRole',
-            message: 'Which team member would you like to add?',
-            choices: [ 'Manager', 'Engineer', 'Intern', 'Generate HTML'],
-          },       
-          {
-            type: 'input',
-            name: 'id',
-            message: 'What is the team member employee ID?',
-          },
-          {
-            type: 'input',
-            name: 'name',
-            message: 'What is the team member name?',
-          },
-          {
-            type: 'input',
-            name: 'email',
-            message: 'What is the team member email?',
-          }])
-         // if manager include office number & offer to add next
-          .then(({pickRole, id, name, email}) => {
-            if(pickRole ==='Manager') {
-              return inquirer
-                  .prompt([
-                    {
-                      type: 'input',
-                      name: 'officePhone',
-                      message: 'What is your office phone number?',
-                    }, 
-                    {
-                      type: 'confirm',
-                      name: 'addAnother',
-                      message: 'Would you like to add another team member?',
-                      default: false
-                    }
-                  ]) 
-                  .then(({officePhone, addAnother}) => {
-                        manager.push(new Manager (name, id, email, officePhone))
-                        console.log(addEmployee)
-                        if (addAnother) {
-                          return promptUser();
-                        }
-                      })
-              // if engineer include Github + repository      
-              } else if (pickRole === "Engineer"){
-                return inquirer
-                  .prompt([
-                  {
-                    type: 'input',
-                    name: 'gitHub',
-                    message: 'What is your github username?',
-                  },
-                  {
-                    type: 'input',
-                    name: 'githubLink',
-                    message: 'What is your github repository url?',
-                  }])
+    {
+      type: 'list',
+      name: 'pickRole',
+      message: 'Which team member would you like to add?',
+      choices: ['Manager', 'Engineer', 'Intern', 'Generate HTML'],
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is the team member employee ID?',
+    },
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the team member name?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is the team member email?',
+    }])
+    // if manager include office number & offer to add next
+    .then(({ pickRole, id, name, email }) => {
+      if (pickRole === 'Manager') {
+        return inquirer
+          .prompt([
+            {
+              type: 'input',
+              name: 'officePhone',
+              message: 'What is your office phone number?',
+            },
+            {
+              type: 'confirm',
+              name: 'addAnother',
+              message: 'Would you like to add another team member?',
+              default: false
+            }
+          ])
+          .then(({ officePhone, addAnother }) => {
+            console.log('add another: ', addAnother);
+            manager.push(new Manager(name, email, id, officePhone))
+            console.log(addEmployee)
+            if (addAnother) {
+              return promptUser();
+            }
+            else {
+              console.log('add another false');
+            }
+            // TODO fix
+          })
+        // if engineer include Github + repository      
+      } else if (pickRole === "Engineer") {
+        return inquirer
+          .prompt([
+            {
+              type: 'input',
+              name: 'gitHub',
+              message: 'What is your github username?',
+            },
+            {
+              type: 'input',
+              name: 'githubLink',
+              message: 'What is your github repository url?',
+            },
+            {
+              type: 'confirm',
+              name: 'addAnother',
+              message: 'Would you like to add another team member?',
+              default: false
+            }])
 
-              .then(({gitHub, githubLink, addAnother}) => {
-                engineer.push(new Engineer(name, id, email, gitHub, githubLink))
-                console.log(addEmployee)
-                if (addAnother) {
-                  return promptUser();
-                }
-              })
+          .then(({ gitHub, githubLink, addAnother }) => {
+            engineer.push(new Engineer(name, email, id, gitHub, githubLink))
+            console.log(addEmployee)
+            if (addAnother) {
+              return promptUser();
+            }
+            else {
+              console.log('add another false');
+            }
+          })
 
-            } else if (pickRole === "Intern"){
-              return inquirer
-              .prompt([
+      } else if (pickRole === "Intern") {
+        return inquirer
+          .prompt([
             // if intern include school //child 3
             {
               type: 'input',
               name: 'school',
               message: 'What is your school name?',
+            },
+            {
+              type: 'confirm',
+              name: 'addAnother',
+              message: 'Would you like to add another team member?',
+              default: false
             }])
 
-            .then(({school, addAnother}) => {
-              intern.push(new Intern(name, id, email, school))
-              if (addAnother) {
-                return promptUser();
-              }
-             else  {
+          .then(({ school, addAnother }) => {
+            intern.push(new Intern(name, email, id, school))
+            if (addAnother) {
+              return promptUser();
+            }
+            else {
               //build the html page 
-                console.log("building html")
-              }
-            })
-          }
-        })
+              console.log("building html")
+            }
+          })
       }
+    });
+}
 
 // function to write HTML file
 // syntax: fs.writeFile( file, data, options, callback )
-function writeToFile(fileName, data) {
-    fs.writeFile('./dist/index.html', generateHTML(data), (err) =>
+function writeToFile(data) {
+  fs.writeFile('./output-index.html', generateHTML(data), (err) =>
     err ? console.log(err) : console.log('Success!!!'))
 };
 
 // function to initialize app
 function init() {
-//   // inquirer 
-//   // .prompt()
-//   // .then ((data) => writeToFile(data.title, data))
- promptUser()
-  .then(data => {
-    return writeToFile(addEmployee)
-  })
-   .then (data =>{
-     return fs.writeFile(data)
-   })
+  //   // inquirer 
+  //   // .prompt()
+  //   // .then ((data) => writeToFile(data.title, data))
+  promptUser()
+    .then(data => {      
+      return writeToFile(addEmployee);
+    });
+    
 };
 
-   // Function call to initialize app 
+// Function call to initialize app 
 init();
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,66 +1,152 @@
 // starter code
 // TODO: update html content & add cards
 
-function generateHTML(data) {
-    return `${data.title}
-    
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-      <title>Document</title>
-    </head>
-    <body>
-    <div class="card-group">
-      <div class="jumbotron-fluid">
-        <div class="card m-3 jumbotron-fluid">
-         <div class="card m-2" style = "max-width:18rem;">
-         <div class="card-body">
-      <!-- // Employee -->
-          <h5 class="card-title">Role:${data.title}</h5>
-          <p class="card-text">Name:${data.name} </p>
-          <p class="card-text">Email: ${data.email} </p> 
-        <!-- // Manager + override Role-->
-          <p class="card-text">Office Number:${data.officePhone} </p>
-        <p class="card-text"><small class="text-muted">ID:${data.employeeID}</small></p>
-    
-          </div>
-          </div>
-          <div class="card m-2" style = "max-width:18rem;">
-            <div class="card-body">
-              <div class="card-body">
-                <!-- // Employee -->
-                    <h5 class="card-title">Role:${data.title}</h5>
-                    <p class="card-text">Name:${data.name}</p>
-                    <p class="card-text">Email:${data.email}</p> 
-          <!-- // Engineer + override Role-->
-          <p class="card-text">GitHub Username: ${data.userName} </p>
-          <p class="card-text">GitHub Link:${data.githubLink} </p>
-        <p class="card-text"><small class="text-muted">ID:${data.employeeID}</small></p>
-    
-          </div>
-        </div>
-       
-        <div class="card m-2" style = "max-width:18rem;">
-          <div class="card-body">
-            <div class="card-body">
-              <!-- // Employee -->
-                  <h5 class="card-title">Role:${data.title}</h5>
-                  <p class="card-text">Name: </p>
-                  <p class="card-text">Email: </p>
-          <!-- // Intern + override Role-->
-          <p class="card-text">School: </p>
-        <p class="card-text"><small class="text-muted">ID: ${data.employeeID}</small></p>
-        </div>
-        </div>
-      </div>
-    </div>
-    </body>
-    </html>
-`;
+function addManager(manager) {
+
+  // TODO: fix the css classes and UI.
+  // this is an example UI just to get things working!
+  // function to add manager to html
+  const output = `
+  <div class="card" style="width: 18rem;"> 
+  <div class="card-body">
+    <h5 class="card-title card-header">Manager</h5>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Id: ${manager.id}</li>
+      <li class="list-group-item">Name: ${manager.name}</li>
+      <li class="list-group-item">Email: ${manager.email}</li>
+      <li class="list-group-item">Office phone: ${manager.officePhone}</li>      
+    </ul>    
+  </div>
+</div>
+  `;
+
+  return output;
+
 }
+
+function addEngineer(engineer) { 
+
+  const output = `<div class="card" style="width: 18rem;">`
+    + `<div class="card-body">`
+    + `<h5 class="card-title card-header">Engineer</h5>`
+    + `<ul class="list-group list-group-flush">`
+    + `<li class="list-group-item">Id: ${engineer.id}</li>`
+    + `<li class="list-group-item">Name: ${engineer.name}</li>`
+    + `<li class="list-group-item">Email: ${engineer.email}</li>`
+    + `<li class="list-group-item">Github: ${engineer.gitHub}</li>`
+    + `<li class="list-group-item">Github Link: ${engineer.githubLink}</li>`
+    +`</ul></div></div>`;
+  return output;
+
+}
+
+function addInter(intern) {
+
+  const output = `<div class="card" style="width: 18rem;">`
+  + `<div class="card-body">`
+  + `<h5 class="card-title card-header">Intern</h5>`
+  + `<ul class="list-group list-group-flush">`
+  + `<li class="list-group-item">Id: ${intern.id}</li>`
+  + `<li class="list-group-item">Name: ${intern.name}</li>`
+  + `<li class="list-group-item">Email: ${intern.email}</li>`
+  + `<li class="list-group-item">School: ${intern.school}</li>`
+  +`</ul></div></div>`;
+
+return output;
+
+ }
+
+
+function generateHTML(data) {
+
+  let htmlString = ` <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Document</title>
+  </head>
+  <body>`;
+
+  for (const m of data.manager) {
+    htmlString += addManager(m);
+  }
+
+  for (const e of data.engineer) {
+    htmlString += addEngineer(e);
+  }
+
+  for (const i of data.intern) {
+    htmlString += addInter(i);
+  }
+
+  htmlString += `</body></html>`;
+
+  return htmlString;
+}
+
+
+// function generateHTMLDeprecated(data) {
+//   return `${data.title}
+    
+//     <!DOCTYPE html>
+//     <html lang="en">
+//     <head>
+//       <meta charset="UTF-8">
+//       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+//       <title>Document</title>
+//     </head>
+//     <body>
+//     <div class="card-group">
+//       <div class="jumbotron-fluid">
+//         <div class="card m-3 jumbotron-fluid">
+//          <div class="card m-2" style = "max-width:18rem;">
+//          <div class="card-body">
+//       <!-- // Employee -->
+//           <h5 class="card-title">Role:${data.title}</h5>
+//           <p class="card-text">Name:${data.name} </p>
+//           <p class="card-text">Email: ${data.email} </p> 
+//         <!-- // Manager + override Role-->
+//           <p class="card-text">Office Number:${data.officePhone} </p>
+//         <p class="card-text"><small class="text-muted">ID:${data.employeeID}</small></p>
+    
+//           </div>
+//           </div>
+//           <div class="card m-2" style = "max-width:18rem;">
+//             <div class="card-body">
+//               <div class="card-body">
+//                 <!-- // Employee -->
+//                     <h5 class="card-title">Role:${data.manager}</h5>
+//                     <p class="card-text">Name:${data.name}</p>
+//                     <p class="card-text">Email:${data.email}</p> 
+//           <!-- // Engineer + override Role-->
+//           <p class="card-text">GitHub Username: ${data.userName} </p>
+//           <p class="card-text">GitHub Link:${data.githubLink} </p>
+//         <p class="card-text"><small class="text-muted">ID:${data.employeeID}</small></p>
+    
+//           </div>
+//         </div>
+       
+//         <div class="card m-2" style = "max-width:18rem;">
+//           <div class="card-body">
+//             <div class="card-body">
+//               <!-- // Employee -->
+//                   <h5 class="card-title">Role:${data.title}</h5>
+//                   <p class="card-text">Name: </p>
+//                   <p class="card-text">Email: </p>
+//           <!-- // Intern + override Role-->
+//           <p class="card-text">School: </p>
+//         <p class="card-text"><small class="text-muted">ID: ${data.employeeID}</small></p>
+//         </div>
+//         </div>
+//       </div>
+//     </div>
+//     </body>
+//     </html>
+// `;
+// }
 
 // export html file
 module.exports = generateHTML;
